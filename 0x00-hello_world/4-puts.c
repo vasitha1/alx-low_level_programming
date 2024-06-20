c687807191e13271faaa4e16412df8a9168a622f
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-20 16:06:07 -0700

    Write a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main: entry point
+ *
+ * Description:write a program to that prints a sentence returns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
