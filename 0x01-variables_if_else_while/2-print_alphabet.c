8ee53507df8268606016bf988b0293f642d08c92
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-21 16:09:46 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * char alphabet keeps count of the alphabet
+ *
+ * Description: Creates a game that prints all letters of the alphabet
+ * Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	/**
+	 * creating a loop that will print the subsequent letter at each cycle
+	*/
+	char alphabet = 'a';
+
+	while (alphabet < 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}
