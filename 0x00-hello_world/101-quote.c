62c5c4295936aa003abb532f789628c711acd7d5
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-20 18:02:50 -0700

    Write a C program that prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main -entry point
+ *
+ * Description: Prints a message to standard error
+ * Return: always 1 (success)
+*/
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
+	return (1);
+}
