4b112468cbbc4a77534201c3ca937dd8245628e7
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-21 19:17:43 -0700

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * int hex: To track the number
+ *
+ * Description: To know print headecimal numbers
+ * Return: Always 0 (sucess)
+*/
+int main(void)
+{       
+	char hex[] = "0123456789abcdef";
+
+	int i = 0;
+	while (hex < 16)
+	{
+		putchar(hex[i]);
+		hex++;
+	}
+	putchar('\n');
+	return (0);
+}
