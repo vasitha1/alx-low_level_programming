ee61b33cffb923c8b0d7a7c5f1b43e482fa8d8e1
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-25 09:39:01 -0700

    Write a program that prints _putchar, followed by a new line. Without using standard libraries

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,14 @@
 #include <unistd.h>/*declaring library for function write*/
 
+
 /*creating a header guard before defining it*/
 #ifndef MAIN_H
 #define MAIN_H
 
 /*define functions*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 #endif
