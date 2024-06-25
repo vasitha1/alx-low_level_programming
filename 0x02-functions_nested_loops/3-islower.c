da09785a3ed7c13ed0120495a1e4bcaafdc5e60d
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-25 16:48:31 -0700

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower -Checks if characters are lower case or not
+ *
+ * Description: Verifies if it is between a and z
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+int _islower(int c)
+{
+	if (int c >= 'a' && int c <= 'z')
+	{
+		return (1);
+	}
+	return ('0');
+}
