d4d0e8d41ae2c0def016aa2b07971e273ada8784
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-25 19:05:57 -0700

    Write a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last - prints last digit of a number
+ *@n: Is an int that enters to the function for verification
+ *
+ * Description: The program prints the last digit by finding the % 10
+ * Return: Always last digit
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
