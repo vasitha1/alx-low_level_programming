6784e10841d27a1b5bf3519ca8b25c4b0fa4601e
Vasitha Yong <sulemfelsi@gmail.com>
2024-06-25 17:53:51 -0700

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,9 +9,10 @@
 */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
-	{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		
 		return (1);
-	}
-	return (0);
+	else
+	
+		return (0);
 }
