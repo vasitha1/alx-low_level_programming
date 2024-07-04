4497de85888e6e5238352d2cfdfb35b42dd48f16
Vasitha Yong <sulemfelsi@gmail.com>
2024-07-04 07:12:20 -0700

    Write a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include  "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: string input parametter to be appended
+ * @src: string input parametter that will be sucessfully added to @dest
+ * @n: Max number of bytes to be used from src
+ *
+ * Description: The program concatenates two strings using at most
+ * at most n bytes from src function that concatenates two strings.
+ * Always 0 (sucess)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	char *concatinate = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+	{
+		if (n < 0)
+			break;
+
+		else
+		{
+			*dest = *src;
+			src++;
+			dest++;
+			n--;
+		}
+
+		*dest = '\0';
+	}
+	return (concatinate);
+}
