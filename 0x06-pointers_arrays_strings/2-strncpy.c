93015a398f082ca11df64d72ebc5d426448534a8
Vasitha Yong <sulemfelsi@gmail.com>
2024-07-04 12:15:33 -0700

    Write a function that copies a string.and works exactly like strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include  "main.h"
+
+/**
+ * _strncpy -  function that copies a string.
+ * @dest: Innitially empty string to be appended
+ * @src: String to be copied
+ * @n: Int parameter for representing maximum number of bites to be copied
+ *
+ * Return: Copied string (sucess)
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *copied = dest;
+	int i = 0;
+
+	while (*src != '\0' && i <= n)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		i++;
+	}
+	while (i < n)
+	{
+		*dest = '\0';
+		i++;
+	}
+
+	return (copied);
+}
