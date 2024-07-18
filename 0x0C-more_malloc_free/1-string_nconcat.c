47e195dc5cbfddf2eda600955f38e5335f11c0b7
Vasitha Yong <sulemfelsi@gmail.com>
2024-07-18 12:17:43 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	j = 0;
 
-	while (s1[i] != '\0')
+	while (s1 != NULL && s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2 != NULL && s2[j] != '\0')
 	{
 		j++;
 	}
