a7af9a387ecf2410a941d903c7ecd48b9f07447e
Vasitha Yong <sulemfelsi@gmail.com>
2024-08-01 15:37:39 -0700

    Task 0: Write a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t j;
+	int j;
 
 	j = 0;
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,12 +3,22 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
-typedef struct list_t{
+/**
+ * struct list_s - singly linked list
+ * @str: string (data parameter)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * for Alx project
+ */
+typedef struct list_s
+{
 	char *str;
 	unsigned int len;
-	struct list_t *next;
-	}list_t;
-int _putchar(char c);
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 #endif
