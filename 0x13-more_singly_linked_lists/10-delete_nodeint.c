1c568d50c419aaf012a53c485ae44941022b896b
Vasitha Yong <sulemfelsi@gmail.com>
2024-08-05 18:04:21 -0700

    Write a function that deletes the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (i < (index - 1))
 	{
 		if (ptr == NULL)
-			 return (-1);
+			return (-1);
 		i++;
 		ptr = ptr->next;
 	}
