b7df17770a0456d29bb14d5f2ec2c4370c9da740
Vasitha Yong <sulemfelsi@gmail.com>
2024-08-12 17:08:58 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,11 +15,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	n = 0;
 
-	while (*text_content != '\0')
+	if (text_content != NULL)
 	{
-		n++;
-		text_content++;
-	}
+		while (*text_content != '\0')
+		{
+			n++;
+			text_content++;
+		}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
