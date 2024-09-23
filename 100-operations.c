65013a15521c024c8bd61d69b6ceb937741e7089
Vasitha Yong <sulemfelsi@gmail.com>
2024-09-23 15:59:08 -0700

    I know, you’re missing C when coding in Python. So let’s fix that!

---
+++ 100-operations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers
+ * @a: First number
+ * @b: Second Number
+ *
+ * Return: Adittion of the two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that subtracts two numbers
+ * @a: First number
+ * @b: Second Number
+ *
+ * Return: Subtraction of the two numbers
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * add - function that multiplies two numbers
+ * @a: First number
+ * @b: Second Number
+ *
+ * Return: Multiplication of the two numbers
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * add - function that divides two numbers
+ * @a: First number
+ * @b: Second Number
+ *
+ * Return: Division of the two numbers
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - function that computes the mod of two numbers
+ * @a: First number
+ * @b: Second Number
+ *
+ * Return: Modulus of the two numbers
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
